4b18c10fbde00dc18859bf83961805065dd053b3
Makeba Waddy <153320339+makebashangwe@users.noreply.github.com>
2023-12-10 16:44:35 -0500

    Add files via upload

---
+++ Practicing/ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Runtime.CompilerServices;
+
+namespace UserNamePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string correctPassword = "";
+            string enteredPassword;
+            int guesses = 3;
+            int minAttempts = 0;
+            char at = '@';
+
+            Console.Write("Welcome. Please enter your username: ");
+            string name = Console.ReadLine();
+            string username = at + name;
+            Console.WriteLine("Your username is " + username);
+
+            Console.WriteLine(username + ", please create a password: ");
+            correctPassword = Console.ReadLine();
+            Console.WriteLine("Hope you wrote it down. Let's login? (yes/no): ");
+            string userInput = Console.ReadLine();
+            bool continueLogin = userInput.ToLower() == "yes";
+
+            if (continueLogin)
+            {
+                Console.Clear();
+                string enteredUsername;
+
+               do
+                {
+                    Console.WriteLine("Enter your username: ");
+                    enteredUsername = Console.ReadLine();
+                    if (enteredUsername != name)
+                    {
+                        Console.Clear();
+                        Console.WriteLine(enteredUsername + " is not a username. Please enter a valid username.");
+                    }
+                }
+                while (enteredUsername != name);
+
+
+                if (enteredUsername == name)
+                {
+                    do
+                    {
+                        Console.WriteLine("Password: ");
+                        Console.WriteLine($"Guesses Remaining: " + guesses);
+                        enteredPassword = Console.ReadLine();
+                        if (enteredPassword != correctPassword)
+                        {
+                            guesses--;
+                        }
+                        if (guesses < minAttempts)
+                        {
+                            Console.WriteLine($"Exceeded maximum attempts. Access Denied.");
+                            return;
+                        }
+
+                    }
+                    while (correctPassword != enteredPassword);
+
+                    Console.WriteLine("Correct Password! Access granted.");
+                }
+
+            }
+
+            else
+            {
+                Console.WriteLine("Are you sure? ");
+                string userInputsure = Console.ReadLine();
+                while (0 < 1)
+                {
+                    Console.WriteLine("Are you sure? ");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Practicing/Practice2/Practice2/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Text;
+
+namespace RockPaperScissorsGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            String player;
+            String computer;
+            String answer;
+
+            while (playAgain)
+            {
+                player = "";
+                computer = "";
+                answer = "";
+
+                while (player != "ROCK" & player != "PAPER" & player != "SCISSORS")
+                {
+                    Console.Write("ENTER ROCK, PAPER, or SCISSORS: ");
+                    player = Console.ReadLine();
+                    player = player.ToUpper();
+
+                    Console.WriteLine("Player: " + player);
+                }
+            
+                
+                switch (random.Next(1, 4))
+                {
+                     case 1:
+                        computer = "ROCK";
+                        break;
+                    case 2:
+                        computer = "PAPER";
+                        break;
+                    case 3:
+                        computer = "SCISSORS";
+                        break;
+                }
+
+                Console.WriteLine("Computer: " + computer);
+
+
+                switch (player)
+                {
+                    case "ROCK":
+                       if (computer == "ROCK") 
+                        {
+                            Console.WriteLine("It's a draw");
+                        }
+                        else if (computer == "PAPER") 
+                        {
+                            Console.WriteLine("You Lose!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You won!");
+                        }
+                        break;
+                    case "PAPER":
+                     if (computer == "ROCK") 
+                        {
+                            Console.WriteLine("You Won!");
+                        }
+                        else if (computer == "PAPER") 
+                        {
+                            Console.WriteLine("It's a draw!");
+                        }
+                        else 
+                        {
+                            Console.WriteLine("You lost!");
+                        }
+                        break;
+
+                    case "SCISSORS":
+                         if (computer == "ROCK") 
+                        {
+                            Console.WriteLine("You Lost!");
+                        }
+                        else if (computer == "PAPER") 
+                        {
+                            Console.WriteLine("You Won!!");
+                        }
+                        else 
+                        {
+                            Console.WriteLine("It's a draw!");
+                        }
+                        break;
+                    }
+                Console.Write("Would you like to play again (Y/N)");
+                answer = Console.ReadLine();
+                answer = answer.ToUpper();
+
+                if (answer == "Y")
+                {
+                    playAgain = true;
+                }
+                else
+                {
+                    playAgain = false;
+                    Console.Write("Bye, Loser >:)");
+
+                }
+
+                }
+            }
+        }
+    }
